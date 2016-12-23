ed95b7f319501f69357830c68b31002ff71d36a4
think2011 <452125301@qq.com>
2016-12-23 11:59:29 +0800

    refactor getters

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,118 +1,120 @@
-import {convertRem} from '../assets/js/common'
+export default {
+    shopUrl ({settings}) {
+        return `//shop.m.taobao.com/shop/shopIndex.htm?shop_id=${settings.shopId}`
+    },
 
-export const shopUrl  = ({settings}) => {
-    return `//shop.m.taobao.com/shop/shopIndex.htm?shop_id=${settings.shopId}`
-}
-export const gameData = ({gameData}) => gameData
+    gameData ({gameData}) {
+        return gameData
+    },
 
-export const bgStyle = ({bg, type}) => {
-    let style = null
+    bgStyle ({bg, type}) {
+        let style = null
 
-    if (type === 'mobile') {
-        style = {
-            ...bg.style,
-            height          : hotcss.px2rem(parseFloat(bg.style.height), 750) + 'rem',
-            backgroundImage : `url(${bg.src})`,
-            backgroundRepeat: 'no-repeat',
-            backgroundSize  : '100%',
-            width           : '16rem'
-        }
-    } else {
-        style = {
-            ...bg.style,
-            width     : '100%',
-            background: `url(${bg.src}) no-repeat center top`
-        }
-    }
-
-    return style
-}
-
-export const containerStyle = ({type}) => {
-    let style = {}
-
-    if (type === 'pc') {
-        style = {
-            width   : '990px',
-            height  : '100%',
-            position: 'relative',
-            margin  : ' 0 auto'
-        }
-    }
-
-    return style
-}
-
-export const viewType = ({type}) => {
-    return type
-}
-
-
-export const nextIndex = ({modules}) => {
-    let zIndex = 1
-
-    modules.forEach((item, index) => {
-        if (item.children) {
-            zIndex += item.children.length
+        if (type === 'mobile') {
+            style = {
+                ...bg.style,
+                height          : hotcss.px2rem(parseFloat(bg.style.height), 750) + 'rem',
+                backgroundImage : `url(${bg.src})`,
+                backgroundRepeat: 'no-repeat',
+                backgroundSize  : '100%',
+                width           : '16rem'
+            }
         } else {
-            zIndex++
+            style = {
+                ...bg.style,
+                width     : '100%',
+                background: `url(${bg.src}) no-repeat center top`
+            }
         }
-    })
 
-    return zIndex
-}
+        return style
+    },
 
-export const activityStatus = ({act}) => {
-    let status = {
-        type: act.activityStatus,
-        desc: null
-    }
+    containerStyle ({type}) {
+        let style = {}
 
-    if (act.activityStatus.match(/Ready|Paused/g)) {
-        status.desc  = '活动未开始'
-        status.class = 'waiting'
-    }
-    else if (act.activityStatus == 'Ended') {
-        status.desc  = '活动已结束'
-        status.class = 'end'
-    }
-    else {
-        status.desc  = '活动进行中'
-        status.class = ''
-    }
+        if (type === 'pc') {
+            style = {
+                width   : '990px',
+                height  : '100%',
+                position: 'relative',
+                margin  : ' 0 auto'
+            }
+        }
 
-    return status
-}
+        return style
+    },
 
-export const prizes = ({act}) => {
-    return act.giftsJson
-}
+    viewType ({type}) {
+        return type
+    },
 
-export const tasksStatus = ({act}, {tasksStatusMap}) => {
-    let result = {
-        shopping: act.extraCount.itemCartEnabled,
-        collect : act.extraCount.itemFavorEnabled,
-        share   : act.extraCount.shareEnabled,
-        buy     : act.extraCount.tradePaymentEnabled || act.extraCount.tradeCountEnabled,
-        rate    : act.extraCount.tradeRateEnabled
-    }
+    nextIndex ({modules}) {
+        let zIndex = 1
 
-    for (let p in tasksStatusMap) {
-        if (!tasksStatusMap.hasOwnProperty(p)) continue;
+        modules.forEach((item, index) => {
+            if (item.children) {
+                zIndex += item.children.length
+            } else {
+                zIndex++
+            }
+        })
 
-        result[p] = result[tasksStatusMap[p]]
-    }
+        return zIndex
+    },
 
-    return result
-}
+    activityStatus ({act}) {
+        let status = {
+            type: act.activityStatus,
+            desc: null
+        }
 
-// 因为命名不一样，这个用于编辑器使用
-export const tasksStatusMap = () => {
-    return {
-        shopping: 'shopping',
-        collect : 'collect',
-        share   : 'share',
-        order   : 'buy',
-        praise  : 'rate'
+        if (act.activityStatus.match(/Ready|Paused/g)) {
+            status.desc  = '活动未开始'
+            status.class = 'waiting'
+        }
+        else if (act.activityStatus == 'Ended') {
+            status.desc  = '活动已结束'
+            status.class = 'end'
+        }
+        else {
+            status.desc  = '活动进行中'
+            status.class = ''
+        }
+
+        return status
+    },
+
+    prizes ({act}) {
+        return act.giftsJson
+    },
+
+    tasksStatus ({act}, {tasksStatusMap}) {
+        let result = {
+            shopping: act.extraCount.itemCartEnabled,
+            collect : act.extraCount.itemFavorEnabled,
+            share   : act.extraCount.shareEnabled,
+            buy     : act.extraCount.tradePaymentEnabled || act.extraCount.tradeCountEnabled,
+            rate    : act.extraCount.tradeRateEnabled
+        }
+
+        for (let p in tasksStatusMap) {
+            if (!tasksStatusMap.hasOwnProperty(p)) continue;
+
+            result[p] = result[tasksStatusMap[p]]
+        }
+
+        return result
+    },
+
+    tasksStatusMap () {
+        // 因为命名不一样，这个用于编辑器使用
+        return {
+            shopping: 'shopping',
+            collect : 'collect',
+            share   : 'share',
+            order   : 'buy',
+            praise  : 'rate'
+        }
     }
 }
--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,7 @@ require('babel-polyfill')
 
 import Vue from 'vue'
 import Vuex from 'vuex'
-import * as getters from './getters'
+import getters from './getters'
 import  mutations from './mutations'
 import actions from './actions'
 
